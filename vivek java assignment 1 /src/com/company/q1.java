47ae22f23e828ebbbd2b652009b5af415a2d610a
Vivek19915 <vchoudhary19915@gmail.com>
2021-12-29 18:00:44 +0530

    assignment 1

---
+++ vivek java assignment 1 /src/com/company/q1.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class q1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ vivek java assignment 1 /src/com/company/q2.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Scanner;
+public class q2 {
+    public static void main(String[] args) {
+        int n;
+        Scanner input =  new Scanner(System.in);
+        n = input.nextInt();
+        System.out.println(n);
+
+    }
+}
