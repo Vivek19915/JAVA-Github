46929bc7d6db5cc7a2206ca685c4fb9fbd4c23e7
Vivek19915 <vchoudhary19915@gmail.com>
2021-12-30 22:56:39 +0530

    changs

--- vivek java assignment 2/src/com/company/q4.java
+++ vivek java assignment 2/src/com/company/q4.java
@@ -5,17 +5,32 @@ package com.company;
 import java.util.Scanner;
 
 public class q4 {
-    public static int primeNumber(int num){
-        
+    public static void primeNumber(int num){
+        int count =0;
+        if ( num==0 || num==1){
+            count++;
+        }
+        for (int i =2;i<num ;i++){
+            if (num%i==0 ){
+                count++;
+            }
+        }
+        if (count==0){
+            System.out.println(num+" is a prime number");
+        }
+        else
+            System.out.println(num+" is a non prime number");
+
     }
     public static void main(String[] args) {
         Scanner input  = new Scanner(System.in);
-        System.out.println("Enter size of array");
+        System.out.println("Enter no of integers");
         int size = input.nextInt();
         int []array  = new int[size];
-        System.out.println("enter elements on array");
+        System.out.println("enter elements ");
         for (int i=0;i<size;i++){
             array[i]= input.nextInt();
+            primeNumber(array[i]);
         }
     }
 }
