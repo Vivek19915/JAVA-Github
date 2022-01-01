efb7a22a9abfad7690a46c53f68849cb150aed73
Vivek19915 <vchoudhary19915@gmail.com>
2022-01-01 22:31:10 +0530

    q7

---
+++ vivek java assignment 3 classes and objects/src/com/company/q7.java
@@ -0,0 +1,16 @@
+package com.company;
+
+class monthDay{
+    int month,year;
+
+    public monthDay(int month, int year) {
+        this.month = month;
+        this.year = year;
+    }
+}
+
+public class q7 {
+    public static void main(String[] args) {
+
+    }
+}
