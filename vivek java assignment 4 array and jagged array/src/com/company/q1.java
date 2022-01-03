08803e6ea46d4c804ac0b61c208a349ec155cc47
Vivek19915 <vchoudhary19915@gmail.com>
2022-01-03 17:12:58 +0530

    q1

---
+++ vivek java assignment 4 array and jagged array/src/com/company/q1.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class q1 {
+    public static void main(String[] args) {
+
+        // way to declare string array
+        String [][]array = {{"water"," 100"," 0"}, {"musturd oil"," 90"," - 5"},{"petrol"," 70"," -10"},{"milk"," 100"," 0"}};
+
+    }
+}
