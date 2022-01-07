a614e9cef4e31e22b3a8a3783e93228e4e9d42f1
Vivek19915 <vchoudhary19915@gmail.com>
2022-01-07 13:44:19 +0530

    commit

---
+++ AWT CALCULATOR/src/com/company/calculator.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+class calculator_processing{
+    //CONSTRUCTOR
+    public calculator_processing(){
+        Frame frame = new Frame("CALCULATOR");
+        Label label1 = new Label();
+        Button button1  = new Button("calculate");
+
+
+        button1.setBounds(20,40,100,30);
+
+
+
+
+
+
+
+
+
+
+        frame.add(button1);
+        frame.add(label1);
+        frame.setSize(450,300);
+        frame.setBackground(Color.lightGray);
+        frame.setVisible(true);
+        frame.setLayout(null);
+        //to close app
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                frame.dispose();
+            }
+        });
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+public class calculator {
+    public static void main(String[] args) {
+
+        calculator_processing vivek = new calculator_processing();
+
+    }
+}
--- awt/src/com/company/AWT.java
+++ awt/src/com/company/AWT.java
@@ -14,7 +14,7 @@ class awtapp extends Frame {
         setVisible(true);                     //isse vo visible ho jayega
         setTitle("My first GUI");              //use to set title bar
 
-        // and window listener ka kaam h jo window bani h uspe kaam karna 
+        // and window listener ka kaam h jo window bani h uspe kaam karna
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -24,13 +24,42 @@ class awtapp extends Frame {
     }
 }
 
+
+
+
+
 //ONE MORE WAY OF CREATING CLASS WITHOUT USING EXTENDS
 class awtapp2{
     // always note first you have to make constructer
     public awtapp2(){
         //now we use frame class we use it by making object of it
         Frame frame  = new Frame("My first GUI");
-        frame.setSize(400,200);
+//        note jtne bhi objects bane ge vo ek ke nichhe ek banhege
+//                aur inke beech me ek bhi processing code nhi aana chaiye
+
+        Label label1 = new Label("hello everyone")   ;         //to add text in your but no  one can chage it use label class
+        Button button1  =  new Button("click me");         // now we make object for button to use button class
+        TextField textfiled1  =  new TextField();                 //this is use so that user can enter text in our progra
+
+        textfiled1.setBounds(40 ,120,220,30);
+        textfiled1.setBackground(Color.red);
+        textfiled1.setForeground(Color.yellow);                  //foregrounfd is used to set color of text
+
+
+        label1.setBounds(40,100,220,30);
+        label1.setText("hello vivek");
+
+
+        button1.setBounds(40,40,120,30);    //yaha hamne button1 ke bare me bata diya h
+
+
+        frame.add(button1);                                      //now to add button in frame we use frame add button
+        frame.add(label1);                                    //to add label
+        frame.add(textfiled1);                                    //to add textfiled
+
+
+        frame.setSize(400,300);
+        frame.setLayout(null);    // take jo bhi ham dale vo pure layout par na aa jaye
         frame.setBackground(Color.green);
         frame.setVisible(true);
 
@@ -56,7 +85,7 @@ public class AWT {
 //         or
 //           isme memory leta h
 
-        awtapp vivek  =  new awtapp();
+//        awtapp vivek  =  new awtapp();
 
         awtapp2 amar  = new awtapp2();
     }
