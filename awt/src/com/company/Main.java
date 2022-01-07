e64cb837b2f18ffca5ece8c2e6203b926279f392
Vivek19915 <vchoudhary19915@gmail.com>
2022-01-07 11:36:44 +0530

    awt

---
+++ awt/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+// now we import farame class so  that we can make frame for our app
+import java.awt.*;
+
+// now we extends our main class to use class frame
+//and its fucntion
+public class Main extends Frame {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
